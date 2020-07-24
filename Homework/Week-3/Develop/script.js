70bd0f9ff170341df3012a918895bd51e751f67a
Grant Scriver <grantscriver@gmail.com>
2020-07-23 21:50:25 -0500

    finished my week 3 homework

--- Homework/Week-3/Develop/script.js
+++ Homework/Week-3/Develop/script.js
@@ -1,13 +1,167 @@
 // Assignment Code
+// This section is where I declare variables and arrays for my code
 var generateBtn = document.querySelector("#generate");
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var specialCharacters = [
+  " ",
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Write password to the #password input
+// This sections prompts the user with how many characters they want for their password and which character types to include
+var firstPrompt = prompt(
+  "How many characters would you like your password to contain?"
+);
+if (firstPrompt < 8 || firstPrompt > 128) {
+  alert("Password doesnt meet criteria, Please try again");
+} else {
+  var firstConfirm = confirm("Click ok to confirm including special charcters");
+  var secondConfirm = confirm(
+    "Click ok to confirm including numeric characters"
+  );
+  var thirdConfirm = confirm(
+    "Click ok to confirm including lowercase charcters"
+  );
+  var fourthConfirm = confirm(
+    "Click ok to confirm including uppercase charcters"
+  );
+}
+
+// This seciton takes the information from the prompts and confirms then pushes that into the variable mixedCharacters
+
+var mixedCharacters = [];
+if (firstConfirm) {
+  mixedCharacters.push(specialCharacters);
+}
+if (secondConfirm) {
+  mixedCharacters.push(numbers);
+}
+if (thirdConfirm) {
+  mixedCharacters.push(lowercase);
+}
+if (fourthConfirm) {
+  mixedCharacters.push(uppercase);
+}
+
+writePassword();
+
+//  This is the main function to get and print the password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
 
+// Get random type of character from the set of chosen types by the user
+function randomtype() {
+  var randomIndex = Math.floor(Math.random() * mixedCharacters.length);
+  //var randomPw = randomIndex * firstPrompt;
+  var randomPw = randomIndex;
+  return randomPw;
+}
+// This function has a for loop that takes the information that has been collected from the user and generates a random character based on a random type and random element within that type.
+// It will generate as many characters as the user chose in the beginning.
+function generatePassword() {
+  var passwordchars = "";
+  var currenttype = 0;
+  var lengthofcurrenttype = 0;
+  var randomelement = 0;
+  for (i = 1; i <= firstPrompt; i++) {
+    currenttype = randomtype();
+    lengthofcurrenttype = mixedCharacters[currenttype].length;
+    randomelement = Math.floor(Math.random() * lengthofcurrenttype);
+    passwordchars = passwordchars.concat(
+      mixedCharacters[currenttype][randomelement]
+    );
+  }
+  // This return element takes our final password and sends it back to the writepassword function
+  return passwordchars;
 }
 
 // Add event listener to generate button
--- Homework/Week-3/README.md
+++ Homework/Week-3/README.md
@@ -1,48 +1,7 @@
-# 03 JavaScript: Password Generator
+This website will help you generate a random password. It works by first asking the user if how many characters they want the password to be.
+After the user is prompted with 4 confirmations on whether they would like to include Lowercase, Uppercase, Special characters, or Numbers.
+Depeding on the users response the website will then generate a random password that fits the criteria chosen by the user.
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+![alt text](./assets/Screenshot.jpg "Screenshot of my website")
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-The following image demonstrates the application functionality:
-
-![password generator demo](./Assets/03-javascript-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+https://github.com/grantscriver/My-Portfolio/tree/master/Homework/Week-3/Develop
