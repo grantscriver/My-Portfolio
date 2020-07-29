ed54c65241f24552c3896f565266d3e7c9805de9
Grant Scriver <grantscriver@gmail.com>
2020-07-28 21:41:04 -0500

    Finished Homework

--- Homework/Week-4/Develop/script.js
+++ Homework/Week-4/Develop/script.js
@@ -1,9 +1,14 @@
+// declares all variables for the JavaScript
 var questionList = document.querySelector("#question-list");
+var highscoreform = document.querySelector("#highscoreform");
 var currentQuestion = document.querySelector("#current-question");
-
+var currentScore = document.querySelector("#current-score");
+var timeE1 = document.querySelector(".time");
 var currentQuestionNumber = 0;
-
+var highScores = [];
 var QuestionsAndAnswers = [];
+
+// Declares question and answer variables
 
 QuestionsAndAnswers.push(
   "What does CSS stand for?",
@@ -27,12 +32,34 @@ QuestionsAndAnswers.push(
   "D; Jamsongs"
 );
 
-var Answer_Key = [0, 1, 2];
-var Users_Answers = [];
+// start the timer
+timeStart();
 
+// First function call to initiate quiz
 renderTestQuestion();
 
-// paint the page with the next question
+// Declared variables
+
+var Answer_Key = [0, 1, 2];
+var Users_Answers = [];
+var secondsLeft = 60;
+
+// Starts timer
+
+function timeStart() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeE1.textContent = secondsLeft + " seconds";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      console.log(hi);
+    }
+  }, 1000);
+}
+
+// Renders the questions and answers for the current question
+
 function renderTestQuestion() {
   questionList.innerHTML = "";
   currentQuestion.textContent = QuestionsAndAnswers[currentQuestionNumber * 5];
@@ -66,8 +93,19 @@ function renderTestQuestion() {
   questionList.appendChild(li);
 }
 
+// This function redirects the user to the highscores.html page after the quiz is completed
+
+function renderHighScores() {
+  window.location.href = "./highscores.html";
+  // Investigate why this next line is not executing
+  currentScore.textContent = "hello";
+}
+
+// gets the users chosen answer for current question and keeps track of their score.
+
 questionList.addEventListener("click", function (event) {
   var element = event.target;
+  var numCorrect = 0;
   var Question_count = QuestionsAndAnswers.length / 5;
   if (element.matches("button") === true) {
     var index = element.parentElement.getAttribute("data-index");
@@ -76,13 +114,24 @@ questionList.addEventListener("click", function (event) {
       Users_Answers[currentQuestionNumber] == Answer_Key[currentQuestionNumber]
     ) {
       alert("Correct!");
+      numCorrect += 1;
     } else {
       alert("Incorrect!");
+      secondsLeft -= 15;
     }
   }
+
+  // incrementing the question counter.
+
   currentQuestionNumber += 1;
-  // put if statement that checks if we are done with the questions
+
+  // if its not the end of the tests sends user to next questions otherwise it calculates high scores and sends user to highscores.html
+
   if (currentQuestionNumber < Question_count) {
+    renderTestQuestion();
+  } else {
+    highScores.push(secondsLeft);
+    alert("your score: " + highScores);
+    renderHighScores();
   }
-  renderTestQuestion();
 });
--- Homework/Week-4/README.md
+++ Homework/Week-4/README.md
@@ -1,42 +1,7 @@
-# 04 Web APIs: Code Quiz
+# Code Quiz
 
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+This code presents the user with a start menu. As soon as the user clicks the "Start" the quiz begins. The user will then have 60 seconds to finish the quiz, or they will receive a negative score. If the user answers incorrectly their score will also be deducted by 15 seconds. After the user has completed the quiz they are sent to a high scores page where they can input their initials and see the highest scores.
 
-## User Story
+![alt text](./Assets/Screenshot.jpg)
 
-```
-AS A coding bootcamp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
-
-### Review
-
-You are required to submit the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+https://grantscriver.github.io/My-Portfolio/Homework/Week-4/Develop/index.html
